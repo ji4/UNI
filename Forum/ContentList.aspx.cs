71e10938f6c4fc080f32518a6dec3c4aace20aaf
howdyhi <money@AnonymousdeMacBook-Air.local>
2016-12-26 19:41:15 +0800

    Code 23-14 Page_Load()

--- Forum/ContentList.aspx.cs
+++ Forum/ContentList.aspx.cs
@@ -9,6 +9,7 @@ public partial class ContentList : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //設定DataList的資料原始檔案
+        XmlDataSource1.DataFile = Request.QueryString["filename"] + "file.xml";
     }
 }
